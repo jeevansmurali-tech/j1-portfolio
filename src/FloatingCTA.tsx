26eacabe51be62a6f98c8f1f2e199fb45c70c34e
jeevansmurali-tech <jeevan.s.murali@gmail.com>
2026-03-13 19:41:04 +0530

    Made desktop Floating CTA permanently visible

--- src/FloatingCTA.tsx
+++ src/FloatingCTA.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function FloatingCTA() {
     // This function smoothly scrolls the page down to your contact section
     const scrollToContact = () => {
